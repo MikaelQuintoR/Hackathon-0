fabbdcc4ef26bcad95527e52e8676b2eb38f710f
MikaelQuintoR <MIKAEL.QUINTO@UTEC.EDU.PE>
2024-08-31 17:38:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -60,3 +60,127 @@ elif "/" in op:
     print(division(op))
 elif "*" in op:
     print(multiplicacion(op))
+def Suma( operacion :str):
+    global n1
+    n=""
+    for i in operacion:
+        if i!="+":
+            n+=i
+        else :
+            n1=int(n)
+            n=""
+    n2=int(n)
+    return n1+n2
+
+
+def Resta( operacion :str):
+    global n1
+    n=""
+    for i in operacion:
+        if i!="-":
+            n+=i
+        else :
+            n1=int(n)
+            n=""
+    n2=int(n)
+    return n1-n2
+
+def multiplicacion(operacion :str):
+    global n1
+    n=""
+    for i in operacion:
+        if i!="":
+            n+=i
+        else :
+            n1=int(n)
+            n=""
+    n2=int(n)
+    return n1n2
+
+
+def division( operacion :str):
+    global n1
+    n=""
+    for i in operacion:
+        if i!="/":
+            n+=i
+        else :
+            n1=int(n)
+            n=""
+    n2=int(n)
+    return n1/n2
+
+op = input(str("Ingrese la operacion"))
+
+if"c" in op:
+    op = input(str("Ingrese la operacion"))
+elif "+" in op:
+    print(Suma(op))
+elif "-" in op:
+    print(Resta(op))
+elif "/" in op:
+    print(division(op))
+elif "*" in op:
+    print(multiplicacion(op))
+def Suma( operacion :str):
+    global n1
+    n=""
+    for i in operacion:
+        if i!="+":
+            n+=i
+        else :
+            n1=int(n)
+            n=""
+    n2=int(n)
+    return n1+n2
+
+
+def Resta( operacion :str):
+    global n1
+    n=""
+    for i in operacion:
+        if i!="-":
+            n+=i
+        else :
+            n1=int(n)
+            n=""
+    n2=int(n)
+    return n1-n2
+
+def multiplicacion(operacion :str):
+    global n1
+    n=""
+    for i in operacion:
+        if i!="*":
+            n+=i
+        else :
+            n1=int(n)
+            n=""
+    n2=int(n)
+    return n1*n2
+
+
+def division( operacion :str):
+    global n1
+    n=""
+    for i in operacion:
+        if i!="/":
+            n+=i
+        else :
+            n1=int(n)
+            n=""
+    n2=int(n)
+    return n1/n2
+
+op = input(str("Ingrese la operacion"))
+
+if"c" in op:
+    op = input(str("Ingrese la operacion"))
+elif "+" in op:
+    print(Suma(op))
+elif "-" in op:
+    print(Resta(op))
+elif "/" in op:
+    print(division(op))
+elif "*" in op:
+    print(multiplicacion(op))
