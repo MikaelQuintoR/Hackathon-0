7f8afa878b1908c9ec1cca281947be06237e407c
MikaelQuintoR <MIKAEL.QUINTO@UTEC.EDU.PE>
2024-08-31 17:14:36 -0500

    Create Suma_Resta.py

---
+++ Suma_Resta.py
@@ -0,0 +1,27 @@
+def Suma( operacion :str):
+    global n1
+    n=""
+    for i in operacion:
+        if i!="+":
+            n+=i
+        else :
+            n1=int(n)
+            n=""
+    n2=int(n)
+    return n1+n2
+op=input(str("Ingrese la operacion"))
+print(Suma(op))
+
+def Resta( operacion :str):
+    global n1
+    n=""
+    for i in operacion:
+        if i!="-":
+            n+=i
+        else :
+            n1=int(n)
+            n=""
+    n2=int(n)
+    return n1-n2
+op=input(str("Ingrese la operacion"))
+print(Resta(op))
